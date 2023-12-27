edf942a0c3d55cef69a47460458282e103e8e6b2
TamiresPereira <tamytamysousa@gmail.com>
2023-12-27 17:25:57 -0300

    enum

---
+++ src/oo/composicao/heranca/jogador.java
@@ -0,0 +1,24 @@
+package oo.composicao.heranca;
+
+public class jogador {
+
+    int x;
+    int y;
+    boolean andar(Direcao direcao){
+        switch (direcao){
+            case NORTE:
+                y--;
+                break;
+            case LESTE:
+             x++;
+            break;
+            case SUL:
+                y++;
+                break;
+            case OESTE:
+                x--;
+                break;
+    }
+        return true;
+    }
+}
